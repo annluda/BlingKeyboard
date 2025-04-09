d26cfdda76b13311f6bd53f6200a6a2807b89a42
anld <anld@sugon.com>
2025-04-09 11:37:57 +0800

    Initial Commit

---
+++ BlingKeyboard/BlingKeyboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlingKeyboardApp.swift
+//  BlingKeyboard
+//
+//  Created by ald on 2025/4/9.
+//
+
+import SwiftUI
+
+@main
+struct BlingKeyboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
